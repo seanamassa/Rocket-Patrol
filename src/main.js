5355bef792321339c32f2b4d8f37f862341b0a1a
Sean Massa <samassa@ucsc.edu>
2025-01-17 14:40:17 -0800

    updated files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 let config = {
     type: Phaser.AUTO,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [ Menu, Play ]
 }
 
 let game = new Phaser.Game(config)
+
+//reverse keyboard bindings
+let KeyFIRE, keyRESET, keyLEFT, keyRight
+
+// set UI sizes 
+let borderUISize = game.config.height / 15
+let borderPadding = borderUISize / 3
